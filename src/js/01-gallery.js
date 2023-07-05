fb9fcee911e63f6d0ec59f75eda5513a30af3623
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-07-05 10:14:22 +0300

    added Simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 
 console.log(galleryItems);
+//  console.log(createGalleryItems(galleryItems));
+
+function createGalleryItems(arr){
+   return arr.map(({preview, original, description})=>{return `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`
+ }).join('');
+  
+  } 
+  console.log('Hello')
+  const galleryContainer= document.querySelector('.gallery');
+  console.log('gallay-container',galleryContainer);
+  const galleryMarkup=createGalleryItems(galleryItems);
+  console.log(galleryMarkup)
+  galleryContainer.insertAdjacentHTML('beforeend',galleryMarkup);
+  galleryContainer.addEventListener('click',onClick);
+  
+  function onClick(event){
+    event.preventDefault();
+    console.dir(event.target);
+    if(!event.target.classList.contains('gallery__image')){
+      return
+    }else{
+      const url= event.target.dataset.source;
+       const instance = basicLightbox.create(`
+      <img src="${url}" width="1200" height="900">
+        `)
+     instance.show()
+     document.addEventListener('keydown',(event=>{
+      if(event.code ==='Escape'){
+        instance.close();
+      }
+     }));
+    }
+  }
